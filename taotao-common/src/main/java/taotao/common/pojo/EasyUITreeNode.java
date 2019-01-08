617d900a4a5777cbaddf46fb42712ddc55c32dc2
Administrator <Administrator@192.168.1.100>
2019-01-09 01:59:37 +0800

    添加显示异步Tree

---
+++ taotao-common/src/main/java/taotao/common/pojo/EasyUITreeNode.java
@@ -0,0 +1,33 @@
+package taotao.common.pojo;
+
+/**
+* Title: EasyUITreeNode
+* Description: easyUI 异步treepojo
+* Version:1.0.0  
+* @author Administrator
+* @date 2019年1月7日
+*/
+public class EasyUITreeNode {
+	private long id;
+	private String text;
+	private String state;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	
+}
